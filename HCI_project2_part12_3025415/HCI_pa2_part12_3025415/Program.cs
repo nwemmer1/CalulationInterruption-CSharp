9ff23dca1ac3910efa3cffc3d4fbd0ab1c38ca63
nwemmer1 <35506038+nwemmer1@users.noreply.github.com>
2019-11-11 01:06:04 -0500

    Upload part 1

---
+++ HCI_project2_part12_3025415/HCI_pa2_part12_3025415/Program.cs
@@ -0,0 +1,48 @@
+﻿/* Nathan Wemmer
+ * Project 2 Part 3
+ * Instructions: When pressing 's' to interupt, you have to press 'enter' after because the program
+ * uses ReadLine() method to check when there is a key available. If the key available is not 's', 
+ * then ignore the input.
+ */
+
+using System;
+
+namespace PA2_part12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput = "";
+            while (!userInput.ToLower().Equals("q"))
+            {
+                Console.WriteLine("Please enter an integer or'q' to quit.");
+                userInput = Console.ReadLine();
+                if (!userInput.ToLower().Equals("q"))
+                {
+                    double pi = Math.PI;
+                    double piCalc = pi;
+                    int numberOfTimes = int.Parse(userInput);
+                    bool completed = false;
+
+                    Console.WriteLine("You have asked to calculate the square root " + userInput + " times.");
+                    for (int x = 1; x <= numberOfTimes && (completed != true); x++)
+                    {
+                        Console.WriteLine(x);
+                        piCalc = Math.Sqrt(pi);
+                        if (Console.KeyAvailable)
+                        {
+                            completed = Console.ReadLine().ToLower().Equals("s");
+                        }
+                    }
+                    if (completed != true)
+                    {
+                        Console.WriteLine("The square root of PI is " + Math.Round(piCalc, 3));
+                    }
+                }
+            }
+
+        }
+
+    }
+}
